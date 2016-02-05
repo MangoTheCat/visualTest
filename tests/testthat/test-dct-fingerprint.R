8dcb31ed9b197924922d247a4f976a4b845668af
Gabor Csardi <csardi.gabor@gmail.com>
2016-02-05 17:55:28 +0000

    Try DCT for cross-platform fp on base graphics

---
+++ tests/testthat/test-dct-fingerprint.R
@@ -0,0 +1,15 @@
+
+context("DCT fingerprinting")
+
+test_that("Fingerprint of a base R plot is similar across platforms", {
+
+  tmp <- tempfile(fileext = ".png")
+
+  png(tmp)
+  pairs(iris[1:4], main = "Anderson's Iris Data -- 3 species",
+        pch = 21, bg = c("red", "green3", "blue")[unclass(iris$Species)])
+  dev.off()
+
+  expect_true(isSimilar(tmp, "BA68E7B3948C8936"))
+
+})
